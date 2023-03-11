9b5131e18ec40acc9a5ee92e4a56bb08b1cf5f49
Shivangi <shivangit1111@gmail.com>
2023-03-11 11:46:24 +0530

    feat: add calling

--- app/src/main/java/com/example/imlicitintents/MainActivity.kt
+++ app/src/main/java/com/example/imlicitintents/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.imlicitintents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.imlicitintents.databinding.ActivityMainBinding
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnCall.setOnClickListener {
+
+            val callIntent: Intent = Uri.parse("tel:5551234").let { number ->
+                Intent(Intent.ACTION_DIAL, number)
+            }
+            startActivity(callIntent)
+        }
         setContentView(binding.root)
     }
 }
